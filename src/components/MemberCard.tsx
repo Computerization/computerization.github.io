f2988ec56aa203aaacf6434445aa00b817d539fc
Ye Shu <chshu@protonmail.ch>
2020-04-10 12:42:11 +0000

    Alumni Directory: support email as contact method

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -7,11 +7,12 @@ type Member = {
   readonly website: string;
   readonly github: string;
   readonly linkedin: string;
+  readonly email: string;
   readonly bio: string;
   readonly favoredLink: string;
 };
 
-const UserLink = ({name, image, website, github, linkedin, bio, favoredLink}: Member): ReactElement => (
+const UserLink = ({name, image, website, github, linkedin, email, bio, favoredLink}: Member): ReactElement => (
   <div className={styles.card}>
 
     <div className={styles.header}>
@@ -58,6 +59,16 @@ const UserLink = ({name, image, website, github, linkedin, bio, favoredLink}: Me
             {/* LinkedIn LOGO */}
             {/* Downloaded from https://iconmonstr.com/linkedin-3-svg/ */}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+          </a>
+        </li>
+      }
+      {email &&
+        // If `email` exists
+        <li>
+          <a href={email} key={email}>
+            {/* LinkedIn LOGO */}
+            {/* Downloaded from https://iconmonstr.com/email-1-svg/ */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z" /></svg>
           </a>
         </li>
       }
