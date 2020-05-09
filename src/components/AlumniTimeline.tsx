21c4a4397fc64bc968e44119e3c97a8ce5bcbf13
Ye Shu <chshu@protonmail.ch>
2020-05-09 15:25:58 +0000

    Refactor code for alumni page
    - Add TypeScript type checking for majority of codes
    - **Need Help**: make the types more granular
    - Separate alumniData from code logic

--- src/components/AlumniTimeline.tsx
+++ src/components/AlumniTimeline.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import styles from './AlumniTimeline.module.css';
-import Members from "./Members";
+import MemberList from "./Members";
 
-const AlumniTimeline = ({ alumni }) => {
+import type { alumniData, alumniOfYear } from "../data/alumniData";
+
+type AlumniProps = {
+  readonly alumni: alumniData;
+};
+
+const AlumniTimeline = ({ alumni }: AlumniProps): ReactElement => {
   return (
     <div className={styles.container}>
       <ul className={styles.timeline}>
         {
-          alumni.map(({ classOf, members }) => (
+          alumni.map(({ classOf, members }: alumniOfYear): ReactElement => (
             <li className={styles.event} key={classOf}>
               <label className={styles.icon}></label>
               <div className={styles.body}>
                 <p className={styles.date}>Class of {classOf}</p>
-                <Members members={members} />
+                <MemberList members={members} />
               </div>
             </li>
           ))
--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -3,17 +3,21 @@ import styles from './Members.module.css';
 
 import MemberCard from "./MemberCard";
 
-type Member = {
+export type Member = {
+  // should implement a few custom types later
   readonly name: string;
-  readonly image: string;
-  readonly website: string;
-  readonly github: string;
-  readonly linkedin: string;
-  readonly bio: string;
-  readonly favoredLink: string;
+  readonly bio?: string; // should be shorter than 120 characters
+  readonly image?: string;
+  readonly website?: string;
+  readonly github?: string;
+  readonly linkedin?: string;
+  readonly email?: string; // should begin with "mailto:"
+  readonly favoredLink?: "website" | "github" | "linkedin" | "email";
 };
 
-const MemberList = ({members}: { readonly members: readonly Member[] }): ReactElement => (
+type MembersProps = { readonly members: readonly Member[] };
+
+const MemberList = ({ members }: MembersProps): ReactElement => (
   <div className={styles.alumnusContainer}>
     {members.map(member => (
       // Assume "name" will be unique here
