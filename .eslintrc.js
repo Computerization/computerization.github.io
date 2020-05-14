90c009a7149b0a50992a5d7bfd4772ce0298b24b
Ye Shu <chshu@protonmail.ch>
2020-05-14 12:11:10 +0000

    Edit ESLint Rules
    - Enforce React and TypeScript recommendations
    - Ignore unresolved Docusaurus issues
    - Prevent type warnings in JavaScript files
    - Specify personal tastes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   globals: {
     Atomics: 'readonly',
@@ -18,10 +19,43 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
+    // Allow JSX appearance in files with these extensions
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'import/no-unresolved': [
+      'error',
+      {
+        // built-in imports in Docusaurus v2
+        ignore: ['^@theme', '^@docusaurus'],
+      },
+    ],
+    // These are deemed unnecessary
+    'react/jsx-one-expression-per-line': 'off',
+    'import/extensions': 'off',
   },
+  settings: {
+    'import/resolver': {
+      // resolves the .tsx / .ts imports
+      node: {
+        extensions: ['.js', '.json', '.mjs', '.ts', '.tsx'],
+      },
+    },
+  },
+  overrides: [
+    {
+      // JS files should not be treated as TypeScript
+      // See https://github.com/typescript-eslint/typescript-eslint/issues/109
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        // may need to add more rules
+      },
+    },
+  ],
 };
