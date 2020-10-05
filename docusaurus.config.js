a911d4aa9404694a4eae63b5ca0245f8c789fc04
Sam Zhou <sam@developersam.com>
2020-10-04 22:43:57 -0400

    Change url to computerization.io
    SSL cert is now provisioned. Let's make the final switch.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@
 module.exports = {
   title: 'Computerization',
   tagline: 'C社 << Computerization << 世外信息化社',
-  url: 'https://computerization.github.io',
+  url: 'https://computerization.io',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'Computerization', // Usually your GitHub org/user name.
