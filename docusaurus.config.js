a1d8a1395689d66a7ab2f16c9343b055b8b59fb0
Josh-Cena <sidachen2003@gmail.com>
2021-03-16 23:19:25 +0800

    Change KaTeX distribution

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,8 +76,20 @@ module.exports = {
   },
   stylesheets: [
     {
-      href: '/katex/katex.min.css',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
       type: 'text/css',
+      integrity:
+        'sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X',
+      crossorigin: 'anonymous',
+    },
+  ],
+  scripts: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js',
+      integrity:
+        'sha384-g7c+Jr9ZivxKLnZTDUhnkOnsh30B4H0rpLUpJ4jAIKs4fnJI+sEnkvrMWph2EDg4',
+      crossorigin: 'anonymous',
+      defer: true,
     },
   ],
   presets: [
