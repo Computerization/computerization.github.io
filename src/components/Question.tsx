36f60d90cebe697d60f9f95172579ecf677e2c83
phoeniiix1203 <68604554+phoeniiix1203@users.noreply.github.com>
2021-11-24 22:46:02 +0800

    Add 2021-11-24-network-flow.md (#461)
    * Add 2021-11-24-network-flow.md
    
    * Update blog/2021-11-24-network-flow.md
    
    * Add some sample problems to network-flow.md
    
    * Minor changes
    
    * Fixes
    
    * Last bit!
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Question({ children }: { children: React.ReactNode }) {
-  return <div className="alert alert--info">{children}</div>;
+  return (
+    <p>
+      <div className="alert alert--info">{children}</div>
+    </p>
+  );
 }
